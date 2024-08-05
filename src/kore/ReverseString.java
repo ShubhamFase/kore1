6884d33c4490988b3bd5e186b931ef9e8d3cbe1b
USER <USER@DESKTOP-ID5ANKU>
2024-08-05 22:29:05 +0530

    commiting reverse string to branch java.

---
+++ src/kore/ReverseString.java
@@ -0,0 +1,17 @@
+package kore;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String s1="ShubhamFase";
+		String s2="";
+		
+		for(int i=s1.length()-1;i>=0;i--) 
+		{
+			s2+=s1.charAt(i);
+		}
+		System.out.print(s2);
+	}
+
+}
